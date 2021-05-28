641a5e48ba5ab2d7f2e7609fd2395d27709ea066
Nico De Witte <nico.dewitte@outlook.com>
2021-05-28 10:37:59 +0200

    Add wifi output power as hw setting

--- src/hardware/pcb_v4.h
+++ src/hardware/pcb_v4.h
@@ -15,5 +15,14 @@
   // Digital meter config
   #define METER_REQUEST_PIN         13
 
+  // LEDs
+  #define DATA_LED_PIN              12
+  #define COMM_LED_PIN              5
+
+  // WiFi
+  #define PORTAL_WIFI_OUTPUT_POWER          16
+  #define OPERATIONAL_WIFI_OUTPUT_POWER     18
+    // Default = 20.5
+
 #endif
 
